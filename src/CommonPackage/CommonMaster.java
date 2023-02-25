6e5835a6d1694cc3618707405cd9c136c8d14093
Fatih <faydan80@outlook.com>
2023-02-25 23:05:42 +0100

    In common class but not common! see the file to have more information

---
+++ src/CommonPackage/CommonMaster.java
@@ -0,0 +1,10 @@
+package CommonPackage;
+
+public class CommonMaster {
+
+    // we have succeeded to create some common packages and some branch specified packages and classes
+    // this class is in common Package. but after creating another branch it is not common any more
+    // this class belongs to
+    // MASTER MASTER MASTER
+    //class
+}
