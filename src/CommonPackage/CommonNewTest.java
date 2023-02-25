cec481c16c5a99e926ea11819612faf2101dbb89
Fatih <faydan80@outlook.com>
2023-02-25 23:10:00 +0100

    In common class but not common! see the file to have more information

---
+++ src/CommonPackage/CommonNewTest.java
@@ -0,0 +1,9 @@
+package CommonPackage;
+
+public class CommonNewTest {
+    // we have succeeded to create some common packages and some branch specified packages and classes
+    // this class is in common Package. but after creating another branch it is not common any more
+    // this class belongs to
+    // NewTEST NewTEST NewTEst
+    //class
+}
