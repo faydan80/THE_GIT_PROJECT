e1e7c9d7d3c0b77836217141b6090fbd23cef311
Fatih <faydan80@outlook.com>
2023-02-25 22:30:11 +0100

    yine bir takim degisiklikler! Package, class vb structure manually yazildi.

--- src/CommonPackage/RemoteCommonClass.java
+++ src/CommonPackage/RemoteCommonClass.java
@@ -1,4 +1,13 @@
-// Bu class remote ta olusturuldu. ilk pull denemesi!
+package CommonPackage;
+
+public class RemoteCommonClass {
+    // Bu class remote ta olusturuldu. ilk pull denemesi!
 
 //ilk denemde remote'ta olusturulan file'a .java yazmayi unutmustum
 //simdi düzeltlildi ve commit yazilarak pull edilecek
+
+// yine bir takim degisiklikler
+    //package, class vb structure manually yazildi
+    // sirada commit ve push var
+
+}
