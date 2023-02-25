78d7f9c11306dd313da73af708441a90cdf3beda
Fatih <faydan80@outlook.com>
2023-02-25 22:09:11 +0100

    Ilk deneme! Yeni bir branch olusturmadan önce bir commonPackage ve 2 common class olsutrudum

---
+++ src/CommonPackage/CommonClassBeforeTestBranch1.java
@@ -0,0 +1,9 @@
+package CommonPackage;
+
+public class CommonClassBeforeTestBranch1 {
+    public static void main(String[] args) {
+        System.out.println(".gitignore dosyasina out yazdim ancak bu metodu run edene kadar out yazdigim satirda bir dosya ikono cikmadi");
+
+        System.out.println(" run dan sonra geldi :))");
+    }
+}
