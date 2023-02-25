035c8fe60fba99f6f46414f2c83e8c5eeb9fc3ec
Fatih <faydan80@outlook.com>
2023-02-25 22:51:13 +0100

    NewTest Package is uploaded

---
+++ src/NewTestBranchPackage/NewTestClass1.java
@@ -0,0 +1,7 @@
+package NewTestBranchPackage;
+
+public class NewTestClass1 {
+    //NewTest classi olsuturduktan sonra bir Package olusturdum
+    //bu denem de Package larin da Branch'a özel olup olmadigini görecegiz
+
+}
